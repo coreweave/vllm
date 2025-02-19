90726e9b607b95863766f01a8478a2f6d4d9a187
Sanger Steel <sangersteel@gmail.com>
2025-02-19 11:55:23 -0500

    chore: Use `typing.TYPE_CHECKING` to import `TensorizerConfig` for annotation

--- vllm/lora/request.py
+++ vllm/lora/request.py
@@ -1,11 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import typing
 import warnings
 from typing import Optional
 
 import msgspec
 
 from vllm.adapter_commons.request import AdapterRequest
+
+if typing.TYPE_CHECKING:
+    from vllm.model_executor.model_loader.tensorizer import TensorizerConfig
 
 
 class LoRARequest(
@@ -31,7 +35,7 @@ class LoRARequest(
     lora_local_path: Optional[str] = msgspec.field(default=None)
     long_lora_max_len: Optional[int] = None
     base_model_name: Optional[str] = msgspec.field(default=None)
-    tensorizer_config: Optional = None  # type: TensorizerConfig
+    tensorizer_config: Optional[TensorizerConfig] = None
 
     def __post_init__(self):
         if self.lora_local_path:
