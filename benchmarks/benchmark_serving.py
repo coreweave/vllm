11ffc022101f66b187d5e7e607c45752c4202f88
Sanger Steel <sangersteel@gmail.com>
2025-07-25 19:22:49 -0400

    fix: Avoid using inconsistent way of fetching commit hash

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -1251,9 +1251,11 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+    org = os.getenv("BENCHMARK_WANDB_ORG_NAME") or None
     server_cmd = os.getenv("SERVER_CMD") or None
     project_name = os.getenv("BENCHMARK_WANDB_PROJECT_NAME") or "vllm-benchmark"
-    weave.init(project_name)
+    weave_id = f"{org}/{project_name}" if org else project_name
+    weave.init(weave_id)
     if server_cmd is not None:
         d = parse_server_cmd_to_dict(server_cmd)
         with weave.attributes(d):
